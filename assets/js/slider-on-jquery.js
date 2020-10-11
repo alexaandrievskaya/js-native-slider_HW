678a51dd660986f4165c48b601d7192afe9b9a92
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2020-10-11 17:09:04 +0300

    UPD: ended jQuery carousel

--- assets/js/slider-on-jquery.js
+++ assets/js/slider-on-jquery.js
@@ -1,9 +1,14 @@
-let $container = $('#carousel');
 let $slides = $('.slide');
+let $indContainer = $('.indicators');
+let $indicators = $('.indicator');
+let $pauseBtn = $('#pause-btn');
+let $nextBtn = $('#next-btn');
+let $prevBtn = $('#prev-btn');
+
 let currentSlide = 0;
 let interval = 2000;
 let timerID = null;
-let slidesCount = $slides.lenth;
+let slidesCount = $slides.length;
 let isPlaying = true;
 
 const SPACE = ' ';
@@ -11,19 +16,72 @@ const LEFT_ARROW = 'ArrowLeft';
 const RIGHT_ARROW = 'ArrowRight';
 const FA_PAUSE = '<i class="far fa-pause-circle"></i>';
 const FA_PLAY = '<i class="far fa-play-circle"></i>';
-const FA_PREV = '<i class="fas fa-angle-left"></i>';
-const FA_NEXT = '<i class="fas fa-angle-right"></i>';
 
-let initControls = () => {
-    let $controls = $container.add('div').addClass('controls').addId('controls-container');
 
-    const PAUSE = `<span id="pause-btn" class="control-pause">${this.FA_PAUSE}</span>`;
-    const PREV = `<span id="prev-btn" class="control-prev">${this.FA_PREV}</span>`;
-    const NEXT = `<span id="next-btn" class="control-next">${this.FA_NEXT}</span>`;
+//$indContainer.css('display', 'flex');
+//$('.controls').css('display', 'block');
 
-    
+let gotoNth = (n) => {
+    $($slides[currentSlide]).toggleClass('active');
+    $($indicators[currentSlide]).toggleClass('active');
+    currentSlide = (n + slidesCount) % slidesCount;
+    $($slides[currentSlide]).toggleClass('active');
+    $($indicators[currentSlide]).toggleClass('active');
 };
 
+let gotoNext = () => {
+    gotoNth(currentSlide + 1);
+};
 
+let gotoPrev = () => {
+    gotoNth(currentSlide - 1);
+};
 
-console.log($controls);
+let play = () => {
+    $pauseBtn.html(FA_PAUSE);
+    isPlaying = !isPlaying;
+    timerID = setInterval(gotoNext, interval);
+};
+
+let pause = () => {
+    if(isPlaying) {
+        $pauseBtn.html(FA_PLAY);
+        isPlaying = !isPlaying;
+        clearInterval(timerID);
+    }
+};
+
+let pausePlay = () => isPlaying ? pause() : play();
+
+let prev = () => {
+    pause();
+    gotoPrev();
+};
+
+let next = () => {
+    pause();
+    gotoNext();
+};
+
+$pauseBtn.on('click', pausePlay);
+$nextBtn.on('click', next);
+$prevBtn.on('click', prev);
+
+let indicate = (e) => {
+    let target = e.target;
+
+    if (target.classList.contains('indicator')) {
+        pause();
+        gotoNth(+target.getAttribute('data-slide-to'));
+    }
+};
+
+$indContainer.on('click', '.indicator', indicate);
+
+let pressKey = (e) => {
+    if (e.key === LEFT_ARROW) prev();
+    if (e.key === RIGHT_ARROW) next();
+    if (e.key === SPACE) pausePlay();
+};
+
+$(document).on('keydown', pressKey);
